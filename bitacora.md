2d754cd1a3c30d5dc8627ce9af17fde1430bac2e
Brenda HR <brendalexa19@gmail.com>
2025-02-28 21:39:16 -0500

    V1.3

--- bitacora.md
+++ bitacora.md
@@ -12,3 +12,22 @@ Crear una p+agina que muestre un mapa centrado en mi barrio
 ## Soluciòn 
 Utilizar una biblioteca de un visor geografico: leaflet, openlayers, mapopen
 
+# Problema 4
+Mostrar el poligono de nuestro barrio
+
+## Posible soluciòn
+Buscar geoJSON de terceros
+Cargar el geoJSON usando async await
+
+# Problema 5
+Crear un boton y asignar un manejador de eventos
+
+# Problema 6
+¿Donde esta el geoJSON de los arboles de bogota?
+Arbolado Urbano. Bogotá D.C.
+
+# Problema 7
+Crear el bounding box del barrio
+
+# Problema 8
+Descargar datos filtrados desde la API(servicio REST)
--- program.js
+++ program.js
@@ -4,3 +4,41 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+async function load_polygon(){
+
+    let my_data = await fetch('ingles.geojson');
+    let my_polygon = await my_data.json();
+    
+    L.geoJSON(my_polygon, 
+        {
+            style: {
+                color: 'purple'
+            }
+        }
+    ).addTo(map)
+}
+
+load_polygon();
+
+let btn_trees = document.getElementById("btn_trees");
+
+btn_trees.addEventListener("click", ()=> alert("hola"))
+
+
+async function load_trees(){
+
+    let my_data = await fetch('arboles_ingles.geojson');
+    let my_polygon = await my_data.json();
+    
+    L.geoJSON(my_polygon, 
+        {
+            style: {
+                color: 'green'
+            }
+        }
+    ).addTo(map)
+}
+
+load_trees();
